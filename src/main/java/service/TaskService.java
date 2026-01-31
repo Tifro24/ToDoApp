bedbd9257851b2e14f986428f82994a878073eaf
Tifro24 <ElijahDuma@hotmail.com>
2026-01-31 23:43:36 +0000

    created task service

---
+++ src/main/java/service/TaskService.java
@@ -0,0 +1,65 @@
+package service;
+
+import domain.Task;
+import repository.InMemoryTaskRepository;
+import repository.TaskRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository){
+        this.taskRepository = taskRepository;
+    }
+
+    public void addTask(String title){
+        if(title.isBlank() || title == null){
+            throw new IllegalArgumentException("Title must not be blank");
+        }
+        else{
+            Task task = new Task(title);
+            taskRepository.addTask(task);
+        }
+
+    }
+
+    public List<Task> getAllTasks(){
+        return taskRepository.getAllTasks();
+    }
+
+    public Task getTaskById(UUID taskId){
+        Task task = taskRepository.getTaskById(taskId) ;
+        if(task == null){
+            throw new IllegalArgumentException("Task not found");
+        }
+        else{
+            return task;
+        }
+    }
+
+    public void deleteTask(UUID taskId){
+        Task task = getTaskById(taskId);
+        if(task != null){
+            taskRepository.deleteTask(taskId);
+        }
+    }
+
+    public void updateTaskTitle(String title, UUID taskId){
+        taskRepository.setTaskTitleById(taskId, title);
+    }
+
+    public void setTaskComplete(UUID taskId){
+        taskRepository.setTaskComplete(taskId);
+    }
+
+    public void setTaskIncomplete(UUID taskId){
+        taskRepository.setTaskIncomplete(taskId);
+    }
+
+    public void toggleComplete(UUID taskId){
+        taskRepository.toggleComplete(taskId);
+    }
+}
